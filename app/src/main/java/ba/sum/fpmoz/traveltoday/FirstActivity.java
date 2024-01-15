ec28fda8fa30730f64cb13644a3236defb678057
IvaPetrovicc <85845217+IvaPetrovicc@users.noreply.github.com>
2024-01-15 16:14:38 +0100

    Login/Reg

---
+++ app/src/main/java/ba/sum/fpmoz/traveltoday/FirstActivity.java
@@ -0,0 +1,38 @@
+package ba.sum.fpmoz.traveltoday;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity {
+
+    Button buttonStart;
+    private static final int TIME_INTERVAL = 2000;
+    private long backPressed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressed + TIME_INTERVAL > System.currentTimeMillis()) {
+            super.onBackPressed();
+            return;
+        } else {
+            Toast.makeText(getBaseContext(), "Press back to exit the application", Toast.LENGTH_SHORT).show();
+        }
+        backPressed = System.currentTimeMillis();
+    }
+
+}
--- app/src/main/java/ba/sum/fpmoz/traveltoday/LoginActivity.java
+++ app/src/main/java/ba/sum/fpmoz/traveltoday/LoginActivity.java
@@ -1,6 +1,86 @@
 package ba.sum.fpmoz.traveltoday;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView textViewRegister, textViewForgotPassword;
+
+    private EditText emailEditText, passwordEditText;
+    private Button buttonLogin;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        textViewRegister = findViewById(R.id.textViewRegister);
+        textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        auth = FirebaseAuth.getInstance();
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String pass = passwordEditText.getText().toString();
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!pass.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        passwordEditText.setError("Empty fields are not allowed");
+                    }
+                } else if (email.isEmpty()) {
+                    emailEditText.setError("Empty fields are not allowed");
+                } else {
+                    emailEditText.setError("Please enter correct email");
+                }
+            }
+        });
+
+
+        textViewRegister.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        textViewForgotPassword.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), ForgotPasswordActivity.class);
+            startActivity(intent);
+        });
+
+
+    }
 }
